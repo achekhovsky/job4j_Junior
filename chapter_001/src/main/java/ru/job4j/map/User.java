5b88444e7ec873a29973e0a14f855c83f086de40
achekhovsky <ringrabbit@rambler.ru>
2018-03-08 11:10:19 +0300

    Переопределить только equals [#1004]

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -23,16 +23,38 @@ public class User {
 	}
 	
 	/* 
-	 * @see java.lang.Object#hashCode()
+	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((birthday == null) ? 0 : birthday.hashCode());
-		result = prime * result + children;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		User other = (User) obj;
+		if (birthday == null) {
+			if (other.birthday != null) {
+				return false;
+			}
+		} else if (!birthday.equals(other.birthday)) {
+			return false;
+		}
+		if (children != other.children) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
 	}
 
 	/**
